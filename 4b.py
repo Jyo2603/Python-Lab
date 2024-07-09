9d9f77fb72bb35855281cca22b503c67079b015c
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2024-07-09 13:23:29 +0530

    Create 4b.py

---
+++ 4b.py
@@ -0,0 +1,8 @@
+#4b. Develop a python program to count all the occurrences of vowels, consonants and digits from the given text using Regular expressions.
+
+import re
+
+st = input("Enter the text:")
+v = re.findall('[aeiouAEIOU]', st)
+print("Vowel occurrences =",len(v))
+print("Consonant occurrences =“,len(st)-len(v))
