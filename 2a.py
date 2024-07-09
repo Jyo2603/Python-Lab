a236f28315e8fcd54e9aea34b2a53ede33c41ab5
Jyo2603 <138552575+Jyo2603@users.noreply.github.com>
2024-07-09 13:10:38 +0530

    Create 2a.py

---
+++ 2a.py
@@ -0,0 +1,18 @@
+#2a. Write a python program to display all the prime numbers in the givenrange.
+
+def isPrime(num):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+start = int(input("Enter the start of the interval: "))
+end = int(input("Enter the end of the interval: "))
+
+for i in range(start, end + 1):
+    if isPrime(i):
+        print(i)
